121a530d56388cb3ef3b8d61728cfb98c0bfc2d1
Bernard Alvero <bernard.alvero@gmail.com>
2019-02-28 10:29:15 +0800

    Added Firebase to App, edit index.html title

---
+++ src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBLhTXFU4Y4tpg4GmZosWqfXsrNjYGPKck",
+  authDomain: "ninja-chat-a4a8c.firebaseapp.com",
+  databaseURL: "https://ninja-chat-a4a8c.firebaseio.com",
+  projectId: "ninja-chat-a4a8c",
+  storageBucket: "ninja-chat-a4a8c.appspot.com",
+  messagingSenderId: "197330906659"
+};
+firebase.initializeApp(config);
+
+//export firestore database
+export default firebaseApp.firestore();
