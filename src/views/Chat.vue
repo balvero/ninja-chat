c1d6c82f5d1c04cc224052cb487b10f4513caa1c
Bernard Alvero <bernard.alvero@gmail.com>
2019-03-25 09:29:41 +0800

    Added vue-chat-scroll and added messages stylings

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -3,11 +3,11 @@
     <h2 class="center teal-text">Ninja Chat</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
-          <li>
-            <span class="teal-text">Name</span>
-            <span class="grey-text text-darken-3">message</span>
-            <span class="grey-text time">time</span>
+        <ul class="messages" v-chat-scroll>
+          <li v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{message.name}}:</span>
+            <span class="grey-text text-darken-3">{{message.content}}</span>
+            <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
       </div>
@@ -20,14 +20,36 @@
 
 <script>
 import NewMessage from "@/components/NewMessage.vue";
+import db from "@/firebase/init";
+import moment from "moment";
+
 export default {
   name: "Chat",
   props: ["name"],
   data() {
-    return {};
+    return {
+      messages: []
+    };
   },
   components: {
     NewMessage
+  },
+  created() {
+    let ref = db.collection("messages").orderBy("timestamp");
+
+    ref.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type == "added") {
+          let doc = change.doc;
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            content: doc.data().content,
+            timestamp: moment(doc.data().timestamp).format("lll")
+          });
+        }
+      });
+    });
   }
 };
 </script>
@@ -38,11 +60,28 @@ export default {
 }
 
 .chat span {
-  font-size: 1.4em;
+  font-size: 1.2em;
 }
 
 .chat .time {
   display: block;
-  font-size: 1.2em;
+  font-size: 0.8em;
+}
+
+.messages {
+  max-height: 300px;
+  overflow: auto;
+}
+
+.messages::-webkit-scrollbar {
+  width: 3px;
+}
+
+.messages::-webkit-scrollbar-track {
+  background: #ddd;
+}
+
+.messages::-webkit-scrollbar-thumb {
+  background: #aaa;
 }
 </style>
